6fbc70d818345b432f6ea6b74ed075e6987119f8
gitongalarson <larsonmwendah07@gmail.com>
2024-07-19 12:16:44 +0300

    brave

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+// index.js
+
+// Define the writeCards function
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+
+// Define the countDown function
+function countDown(start) {
+    while (start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
